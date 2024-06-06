7e93824e90249758062a5ec3ab44a68c56067abb
MizanurRahman1234 <fahimmizan24@gmail.com>
2024-06-06 07:27:38 +0600

    updated for picture

--- playground/urls.py
+++ playground/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import medicine_list_view
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', medicine_list_view, name='medicine_list'),
 ]
+urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
